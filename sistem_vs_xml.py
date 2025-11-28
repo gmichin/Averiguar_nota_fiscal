372291919ba9bb6bb568ee66ecff34319d6291d0
Gustavo Massamichi Nakamura <gmassamichi@gmail.com>
2025-11-28 13:14:30 -0300

    save

--- sistem_vs_xml.py
+++ sistem_vs_xml.py
@@ -163,6 +163,8 @@ def verificar_inutilizacao_nota_nao_autorizada(caminhos_eventos, nfe_num):
                         return True
                     if '<xJust>ERRO NO SEFAZ................</xJust>' in conteudo:
                         return True
+                    if '<xJust>MERCADO NAO QUIS RECEBER</xJust>' in conteudo:
+                        return True
                 except Exception:
                     continue
         except Exception:
@@ -354,7 +356,7 @@ def buscar_xml_por_data():
     
 def processar_faturamento_bruto():
     """Processa arquivos CSV para faturamento bruto"""
-    caminho_fechamento = r"S:\hor\excel\fechamento-20251102-20251126.csv"
+    caminho_fechamento = r"S:\hor\excel\fechamento-20251102-20251128.csv"
     caminho_cancelados = r"S:\hor\arquivos\gustavo\can.csv"
     caminho_historico = r"S:\hor\excel\20251102.csv"
     
